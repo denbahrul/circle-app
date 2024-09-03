f6fcc0cd264e94f47bc20e6da8a348015fb33300
denbahrul <ulum0326@gmail.com>
2024-09-03 12:09:08 +0700

    Follows Page UI

--- circle-app-frontend/src/app/pages/follows.tsx
+++ circle-app-frontend/src/app/pages/follows.tsx
@@ -1,10 +1,16 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 import AppLayout from "../../components/layout/app-layout";
+import FollowsTabs from "../../features/follows/components/follows-tabs";
 
 export default function Follows() {
   return (
     <AppLayout>
-      <Box color={"white"}>Follows</Box>
+      <Box mt={4}>
+        <Text padding={4} fontSize={"28px"} fontWeight={700} lineHeight={"28px"}>
+          Follows
+        </Text>
+        <FollowsTabs />
+      </Box>
     </AppLayout>
   );
 }

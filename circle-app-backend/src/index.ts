195cae61f40297ee4eab36ec6eb18b846e76a438
denbahrul <ulum0326@gmail.com>
2024-09-10 23:02:56 +0700

    TypeScript set up with Node.js and Express

---
+++ circle-app-backend/src/index.ts
@@ -0,0 +1,17 @@
+// const express = require("express");
+// const detenv = require("dotenv");
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
--- circle-app-frontend/src/features/follows/components/follows-tabs.tsx
+++ circle-app-frontend/src/features/follows/components/follows-tabs.tsx
@@ -17,17 +17,18 @@ export default function FollowsTabs() {
             fullName="Elon Musk"
             userName="@elonnnn"
             bio="Membagikan segala hal yang berhubungan dengan fotografi dan editing (Lightroom pada khususnya) | Pinned tweet adalah kumpulan thread editing 📸 | Seorang Gooner"
+            isFollow="Follow"
           />
-          <OthersAccountItem image="./profile.png" fullName="Elon Musk" userName="@elonnnn" bio="Post about Product Design • My Experience" />
-          <OthersAccountItem image="./profile.png" fullName="Elon Musk" userName="@elonnnn" bio="Post about Product Design • My Experience" />
-          <OthersAccountItem image="./profile.png" fullName="Elon Musk" userName="@elonnnn" bio="Post about Product Design • My Experience" />
-          <OthersAccountItem image="./profile.png" fullName="Elon Musk" userName="@elonnnn" bio="Post about Product Design • My Experience" />
+          <OthersAccountItem image="./profile.png" fullName="Elon Musk" userName="@elonnnn" bio="Post about Product Design • My Experience" isFollow="Follow" />
+          <OthersAccountItem image="./profile.png" fullName="Elon Musk" userName="@elonnnn" bio="Post about Product Design • My Experience" isFollow="Following" />
+          <OthersAccountItem image="./profile.png" fullName="Elon Musk" userName="@elonnnn" bio="Post about Product Design • My Experience" isFollow="Follow" />
+          <OthersAccountItem image="./profile.png" fullName="Elon Musk" userName="@elonnnn" bio="Post about Product Design • My Experience" isFollow="Follow" />
         </TabPanel>
         <TabPanel display={"flex"} flexDirection={"column"} gap={4}>
-          <OthersAccountItem image="./profile.png" fullName="Najwa Shihab" userName="@najwa" bio="Post about Product Design • My Experience" />
-          <OthersAccountItem image="./profile.png" fullName="Najwa Shihab" userName="@najwa" bio="Post about Product Design • My Experience" />
-          <OthersAccountItem image="./profile.png" fullName="Najwa Shihab" userName="@najwa" bio="Post about Product Design • My Experience" />
-          <OthersAccountItem image="./profile.png" fullName="Najwa Shihab" userName="@najwa" bio="Post about Product Design • My Experience" />
+          <OthersAccountItem image="./profile.png" fullName="Najwa Shihab" userName="@najwa" bio="Post about Product Design • My Experience" isFollow="Following" />
+          <OthersAccountItem image="./profile.png" fullName="Najwa Shihab" userName="@najwa" bio="Post about Product Design • My Experience" isFollow="Following" />
+          <OthersAccountItem image="./profile.png" fullName="Najwa Shihab" userName="@najwa" bio="Post about Product Design • My Experience" isFollow="Following" />
+          <OthersAccountItem image="./profile.png" fullName="Najwa Shihab" userName="@najwa" bio="Post about Product Design • My Experience" isFollow="Following" />
         </TabPanel>
       </TabPanels>
     </Tabs>

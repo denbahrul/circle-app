a55b2dd4b281f114c55a19e9e406448ed1792c22
denbahrul <ulum0326@gmail.com>
2024-09-24 12:07:27 +0700

    Handle form with React Hook Form and form validation with Zod

--- circle-app-frontend/src/features/auth/components/login-form.tsx
+++ circle-app-frontend/src/features/auth/components/login-form.tsx
@@ -1,7 +1,22 @@
 import { Box, Image, Text, Input, Button, FormControl } from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
+import { LoginFormInput, loginSchema } from "../schemas/login";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 export default function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormInput>({
+    resolver: zodResolver(loginSchema),
+  });
+
+  function onSubmit(data: LoginFormInput) {
+    console.log(data);
+  }
+
   const buttonStyle = {
     backgroundColor: "brand.green",
     color: "white",
@@ -13,16 +28,30 @@ export default function LoginForm() {
       <Text as={"h1"} fontSize={28} fontWeight={700} marginY={5}>
         Login to Circle
       </Text>
-      <FormControl display={"flex"} flexDirection={"column"} gap={"12px"}>
-        <Input placeholder="Email/Username*" rounded={8} padding={5} fontSize={14} fontWeight={500} borderColor={"brand.borderAbu"} />
-        <Input placeholder="Password" rounded={8} padding={5} fontSize={14} fontWeight={500} borderColor={"brand.borderAbu"} />
-        <Link to="/forgot-password">
-          <Text textAlign={"end"} fontSize={14}>
-            Forgot password?
-          </Text>
-        </Link>
-        <Button sx={buttonStyle}>Login</Button>
-      </FormControl>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormControl display={"flex"} flexDirection={"column"} gap={"12px"}>
+          <Input {...register("email")} placeholder="Email/Username*" rounded={8} padding={5} fontSize={14} fontWeight={500} borderColor={"brand.borderAbu"} />
+          {errors.email && (
+            <Text fontSize={13} color={"red"}>
+              * {errors.email.message}
+            </Text>
+          )}
+          <Input {...register("password")} placeholder="Password" rounded={8} padding={5} fontSize={14} fontWeight={500} borderColor={"brand.borderAbu"} />
+          {errors.password && (
+            <Text fontSize={13} color={"red"}>
+              * {errors.password.message}
+            </Text>
+          )}
+          <Link to="/forgot-password">
+            <Text textAlign={"end"} fontSize={14}>
+              Forgot password?
+            </Text>
+          </Link>
+          <Button type="submit" sx={buttonStyle}>
+            Login
+          </Button>
+        </FormControl>
+      </form>
       <Text fontSize={14} marginTop={4}>
         Don't have an account yet?{" "}
         <Text as={"span"} color={"brand.green"}>

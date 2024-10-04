00ea4874d277fd6e55c85b2a7071c504906953a1
denbahrul <ulum0326@gmail.com>
2024-10-05 06:48:36 +0700

    Fix : Vercel deployment

--- circle-app-backend/dist/src/middlewares/greeting.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = GreetingMiddleware;
-function GreetingMiddleware(req, res, next) {
-    console.log("Welcome");
-    next();
-}
